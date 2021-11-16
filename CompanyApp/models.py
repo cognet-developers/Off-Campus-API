7486ea67d85752d7a216cab8a572e77906363a32
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2021-11-16 23:08:25 +0530

    update

--- CompanyApp/models.py
+++ CompanyApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class offcampus(models.Model):
     companyname=models.CharField(max_length=25,unique=True)
     location=models.CharField(max_length=25)
-    Time=models.TimeField(auto_now_add=True,null=False,blank=False)
+    Time=models.TimeField(null=False,blank=False)
     Date=models.DateField()
     Post=models.CharField(max_length=25)
     vacancy=models.IntegerField()
